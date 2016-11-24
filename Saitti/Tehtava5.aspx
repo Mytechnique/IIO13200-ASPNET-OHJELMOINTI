82ad2680cfa2df8fa2c2582984568f2c4d2cfde8
Mytechnique <lehtosaarih@gmail.com>
2016-11-24 09:47:34 +0200

    Things done

--- Saitti/Tehtava5.aspx
+++ Saitti/Tehtava5.aspx
@@ -8,8 +8,15 @@
 </head>
 <body>
     <form id="form1" runat="server">
-    <div>
-    
+    <div id="buttons">
+        <asp:Button ID="btnGetAllCustomers" runat="server" Text="Hae kaikki asiakkaat" OnClick="btnGetAllCustomers_Click" />
+        <asp:DropDownList ID="ddlCustomerCountry" runat="server"></asp:DropDownList>
+        <asp:Button ID="btnGetAllCustomersFromCountry" runat="server" Text="Hae asiakkaat valitusta maasta" OnClick="btnGetAllCustomersFromCountry_Click" />
+        <asp:Button ID="btnGetAllCustomersFromAllCountries" runat="server" Text="Hae asiakkaat maittain" />
+    </div>
+
+    <div id="data">
+        <asp:GridView ID="gvAsiakkaat" runat="server"></asp:GridView>
     </div>
     </form>
 </body>
--- Saitti/Tehtava5.aspx.cs
+++ Saitti/Tehtava5.aspx.cs
@@ -7,7 +7,39 @@ using System.Web.UI.WebControls;
 
 public partial class Tehtava5 : System.Web.UI.Page
 {
+    protected static DemoxOyEntities ctx;
     protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack)
+        {
+            ctx = new DemoxOyEntities();
+            FillControls();
+        }
+
+    }
+    protected void GetAllCustomers()
+    {
+        gvAsiakkaat.DataSource = ctx.asiakas.ToList();
+        gvAsiakkaat.DataBind();
+    }
+
+    protected void FillControls()
+    {
+        // Asetetaan DropDownListiin maat vain kerran per maa
+        var result = ctx.asiakas.Select(c => c.maa).Distinct();
+        ddlCustomerCountry.SelectedIndex = -1;
+        ddlCustomerCountry.DataSource = result.ToList();
+        ddlCustomerCountry.DataBind();
+        ddlCustomerCountry.Items.Insert(0, string.Empty); // Lisätään tyhjä rivi ensimmäiseksi valinnaksi sivua ladattaessa
+
+    }
+
+    protected void btnGetAllCustomers_Click(object sender, EventArgs e)
+    {
+        GetAllCustomers();
+    }
+
+    protected void btnGetAllCustomersFromCountry_Click(object sender, EventArgs e)
     {
 
     }
