172747f2b68b7fa5499b64623ddc798f17629623
Mytechnique <lehtosaarih@gmail.com>
2016-11-27 16:39:25 +0200

    Tehtava8 Finished, shows pictures

---
+++ Saitti/Tehtava8.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Tehtava8.aspx.cs" Inherits="Tehtava8" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Elokuvien haku</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <div style="float:left">
+            <asp:ListBox ID="lbTheatres" runat="server" Height="400" OnSelectedIndexChanged="lbTeatterit_SelectedIndexChanged" AutoPostBack="true"></asp:ListBox>
+        </div>
+        <div style="float:left">  
+            <asp:DataList ID="dlMovies" runat="server" RepeatColumns="12" RepeatDirection="Horizontal">
+                <ItemTemplate>
+                   <asp:Image ImageUrl='<%#Eval("MoviePortrait") %>' runat="server" />
+                </ItemTemplate>
+            </asp:DataList>
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Saitti/Tehtava8.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Data;
+using System.Xml;
+using System.Web.UI.WebControls;
+
+public partial class Tehtava8 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            InitTheatres();
+        }
+    }
+
+    protected void InitTheatres()
+    {
+        XmlDocument doc = new XmlDocument();
+        doc.Load("http://www.finnkino.fi/xml/TheatreAreas/");
+        XmlNodeList nodes = doc.SelectNodes("/TheatreAreas/TheatreArea");
+        foreach (XmlNode node in nodes)
+        {
+            lbTheatres.Items.Add(new ListItem(node["Name"].InnerText, node["ID"].InnerText));
+        }
+    }
+
+    protected void lbTeatterit_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        DataTable dt = new DataTable();
+        dt.Columns.Add("MoviePortrait", typeof(string));
+        XmlDocument doc = new XmlDocument();
+        doc.Load("http://www.finnkino.fi/xml/Schedule/?area=" + lbTheatres.SelectedValue.ToString());
+        XmlNodeList nodes = doc.SelectNodes("/Schedule/Shows/Show");
+        foreach (XmlNode node in nodes)
+        {
+            string imgUrl = node.SelectSingleNode("Images/EventSmallImagePortrait").InnerText;
+            dt.Rows.Add(imgUrl);
+        }
+        dlMovies.DataSource = dt;
+        dlMovies.DataBind();
+    }
+}
